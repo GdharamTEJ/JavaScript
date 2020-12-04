7b9f139e2d039549204be956255b70d3f1200001
vandana singandipe <vandana.singandipe@gmail.com>
2020-12-04 21:03:30 +0530

    order confirmation page completed

--- js/checkout.js
+++ js/checkout.js
@@ -2,67 +2,58 @@ var cardContainer = document.getElementById("cart-item-container");
 var totalAmount = document.getElementById("total-amount");
 var numberOfItem = document.getElementById("number-of-item");
 
-var myLocalStorageData = JSON.parse(
-    window.localStorage.getItem("product-list")
-);
+var myLocalStorageData = JSON.parse(window.localStorage.getItem("product-list"));
 console.log(myLocalStorageData);
+/* <div class="item">
+<img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/5649908/2018/5/10/6bfe80cd-2f55-42bc-aa7f-e0d6c9e2ac531525936414747-SASSAFRAS-Women-Blue-Solid-Shirt-Dress-3831525936414532-1.jpg" alt="product-img">
+<div class="detail">
+    <h3>Women Blue Solid Shirt Dress</h3>
+    <p>x1</p>
+    <p>Amount: 5200</p>
+</div> */
+// iPreview, iName, iCount, iPrice
 
-function createItemOnCheckOut(iPreview, iName, iCount, iPrice) {
+
+function createItemOnCheckOut(data) {
     var item = document.createElement("div");
-    item.setAttribute("class", "item");
+    item.className = 'item';
 
     var itemImg = document.createElement("img");
-    itemImg.src = iPreview;
+    itemImg.src = data.preview;
+    item.appendChild(itemImg);
 
     var itemDetail = document.createElement("div");
-    itemDetail.setAttribute("class", "detail");
-
-    var itemName = document.createElement("h3");
-    var itemNameText = document.createTextNode(iName);
-    itemName.appendChild(itemNameText);
-
-    var itemCount = document.createElement("p");
-    itemCountText = document.createTextNode("x" + iCount);
-    itemCount.appendChild(itemCountText);
-
-    var itemPrice = document.createElement("p");
-    var itemPriceText = document.createTextNode("Amount: " + iCount * iPrice);
-    itemPrice.appendChild(itemPriceText);
-
-    itemDetail.appendChild(itemName);
-    itemDetail.appendChild(itemCount);
-    itemDetail.appendChild(itemPrice);
-
-    item.appendChild(itemImg);
+    itemDetail.className = 'detail';
     item.appendChild(itemDetail);
 
+
+    var itemName = document.createElement("h3");
+    itemName.innerHTML = data.title;
+    itemDetail.appendChild(itemName);
+
+    var itemCount = document.createElement("p");
+    itemCount.innerHTML = 'x' + data.count;
+    itemDetail.appendChild(itemCount);
+
+    var itemPrice = document.createElement("p");
+    itemPrice.innerHTML = 'Amount:' + (data.count * data.price);
+    itemDetail.appendChild(itemPrice);
     return item;
 }
 
-for (var z = 0; z < myLocalStorageData.length; z++) {
-    cardContainer.append(
-        createItemOnCheckOut(
-            myLocalStorageData[z].preview,
-            myLocalStorageData[z].title,
-            myLocalStorageData[z].count,
-            myLocalStorageData[z].price
-        )
-    );
+for (var x = 0; x < myLocalStorageData.length; x++) {
+    cardContainer.append(createItemOnCheckOut(myLocalStorageData[x]));
 }
 var cost = 0;
 var counter = 0;
 
 for (var y = 0; y < myLocalStorageData.length; y++) {
-    console.log('count ',
-        myLocalStorageData[y].count);
+    // console.log('count ',myLocalStorageData[y].count);
     counter += myLocalStorageData[y].count;
-    console.log('Totalcount ',
-        counter);
+    // console.log('Totalcount ', counter);
 
     // console.log(counter);
-    cost +=
-        parseInt(myLocalStorageData[y].count) *
-        parseInt(myLocalStorageData[y].price);
+    cost += parseInt(myLocalStorageData[y].count) * parseInt(myLocalStorageData[y].price);
     // console.log(cost);
 }
 totalAmount.innerHTML = cost;
@@ -80,6 +71,7 @@ placeOrder.addEventListener("click", function() {
         counter += myLocalStorageData[i].count;
 
     }
+
     totalAmount.innerHTML = cost;
     numberOfItem.innerHTML = counter;
     console.log('counter new ' + counter);
--- js/index.js
+++ js/index.js
@@ -1,290 +1,205 @@
-// ---------------- Menu Toggle -------
-// localStorage.clear();
+/*Creating index page*/
 
-// Selection of the elements
-var clothingCards = document.getElementById("clothingCards");
-var accessoriesCards = document.getElementById("accessoriesCards");
+var clothingCard = document.getElementById('clothing-card');
+var isAccessory = document.getElementById('accessories-card');
 
-// Create Card Function That Create Card Dynamically
-function createItemCard(id, preview, name, brand, price) {
-    //Create a DIV element with class CARD
-    var cardElement = document.createElement("div");
-    cardElement.setAttribute("class", "card");
-    cardElement.setAttribute("id", id);
+function generateCard(data) {
+    /* <div class="card" id="1">
+        <a href="">
+            <img class='img' src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg" alt="Men Navy Blue Solid Sweatshirt">
+            <div class="details">
+                <h3 class='name' >Men Navy Blue Solid Sweatshirt</h3>
+                <h4 class='brand'>United Colors of Benetton</h4>
+                <h5 class='price'>Rs 2599</h5>
+            </div>
+        </a>
+    </div>  */
 
-    //Create a ANCHOR element with HREF
-    var cardLink = document.createElement("a");
-    cardLink.href = "./product.html?product_id=" + id;
 
-    //Create a IMG-CONTAINER element with class IMG
-    var imgContainer = document.createElement("div");
-    imgContainer.setAttribute("class", "img");
+    var card = document.createElement('div');
+    card.className = 'card';
 
-    //Create a IMG element with SRC
-    var newImgElement = document.createElement("img");
-    newImgElement.src = preview;
+    var productLink = document.createElement('a');
+    card.appendChild(productLink);
+    productLink.href = './product.html?id=' + data.id;
 
-    // Append IMG into IMG-CONTAINER
-    imgContainer.appendChild(newImgElement);
+    var thumbnail = document.createElement('img');
+    thumbnail.src = data.preview;
+    thumbnail.className = 'img';
+    productLink.appendChild(thumbnail);
 
-    //Create a DIV element with class DETAILS
-    var deatils = document.createElement("div");
-    deatils.setAttribute("class", "details");
+    var details = document.createElement('div');
+    details.className = 'details';
+    productLink.appendChild(details);
 
     //Create a H3 element with TEXT-NODE NAME
     var newTitleElement = document.createElement("h3");
-    var newName = document.createTextNode(name);
+    newTitleElement.className = 'name';
+    newTitleElement.innerHTML = data.name;
+    details.appendChild(newTitleElement);
 
-    // Append NAME into H3
-    newTitleElement.appendChild(newName);
-    deatils.appendChild(newTitleElement);
-
-    //Create a H4 element with TEXT-NODE BRAND
     var newBrandElement = document.createElement("h4");
-    var newBrand = document.createTextNode(brand);
+    newBrandElement.className = 'brand';
+    newBrandElement.innerHTML = data.brand;
+    details.appendChild(newBrandElement);
 
-    // Append BRAND into H4
-    newBrandElement.appendChild(newBrand);
-    deatils.appendChild(newBrandElement);
-
-    //Create a H5 element with TEXT-NODE PRICE
     var newPriceElement = document.createElement("h5");
-    var newPriceText = document.createTextNode("Rs ");
-    var newPrice = document.createTextNode(price);
-    newPriceElement.appendChild(newPriceText);
+    newPriceElement.className = 'price';
+    newPriceElement.innerHTML = data.price;
+    details.appendChild(newPriceElement);
 
-    // Append PRICE into H5
-    newPriceElement.appendChild(newPrice);
-    deatils.appendChild(newPriceElement);
 
-    // Append IMG-CONATINER into CARD-LINK
-    cardLink.appendChild(imgContainer);
-    cardLink.appendChild(deatils);
+    if (data.isAccessory === false) {
+        clothingCard.appendChild(card);
 
-    // Append CARD-LINK into CARD-ELEMENT
-    cardElement.appendChild(cardLink);
+    } else if (data.isAccessory === true) {
 
-    // Returning CARD-ELEMENT
-    return cardElement;
-}
-
-// ---------------- Request Data & Create Cards On Home Page -------
-
-function getCardsData() {
-    $.get("https://5d76bf96515d1a0014085cf9.mockapi.io/product", function(data) {
-        var responseData = data;
-        for (var i = 0; i < responseData.length; i++) {
-            if (responseData[i].isAccessory === false) {
-                clothingCards.append(
-                    createItemCard(
-                        responseData[i].id,
-                        responseData[i].preview,
-                        responseData[i].name,
-                        responseData[i].brand,
-                        responseData[i].price
-                    )
-                );
-            } else {
-                accessoriesCards.append(
-                    createItemCard(
-                        responseData[i].id,
-                        responseData[i].preview,
-                        responseData[i].name,
-                        responseData[i].brand,
-                        responseData[i].price
-                    )
-                );
-            }
-        }
-    });
-}
-getCardsData();
-
-// ---------------- Request Data On Card Click To Display Product Page -------
-
-function getProductDetail() {
-    // Get Product Id From URL
-    var searchId = window.location.search.split("=")[1];
-
-    // Get Product Details
-    $.get(
-        "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + searchId,
-        function(data) {
-            var productDetail = data;
-            name = productDetail.name;
-            imageSrc = productDetail.preview;
-            brand = productDetail.brand;
-            price = productDetail.price;
-            description = productDetail.description;
-            photo0 = productDetail.photos[0];
-            photo1 = productDetail.photos[1];
-            photo2 = productDetail.photos[2];
-            photo3 = productDetail.photos[3];
-            photo4 = productDetail.photos[4];
-            photo5 = productDetail.photos[5];
-
-            createProductPage(
-                imageSrc,
-                name,
-                brand,
-                price,
-                description,
-                photo0,
-                photo1,
-                photo2,
-                photo3,
-                photo4,
-                photo5
-            );
-        }
-    );
-}
-getProductDetail();
-
-// ---------------- Create Product Page i.e Inserting Information -------
-
-function createProductPage(
-    imageSrc,
-    name,
-    brand,
-    price,
-    description,
-    img0,
-    img1,
-    img2,
-    img3,
-    img4,
-    img5
-) {
-    // Product Image
-    var productImg = document.getElementById("productImg");
-    productImg.src = imageSrc;
-
-    // Product Name
-    var productName = document.getElementById("name");
-    productName.innerHTML = name;
-
-    // Product Barnd
-    var productBrand = document.getElementById("brand");
-    productBrand.innerHTML = brand;
-
-    // Product Price
-    var productPrice = document.getElementById("price");
-    productPrice.innerHTML = price;
-
-    // Product Description
-    var productDescription = document.getElementById("description");
-    productDescription.innerHTML = description;
-
-    // Product Preview Image 0
-    var photo0 = document.getElementById("img0");
-    photo0.src = img0;
-
-    // Product Preview Image 1
-    var photo1 = document.getElementById("img1");
-    photo1.src = img1;
-
-    // Product Preview Image 2
-    var photo2 = document.getElementById("img2");
-    photo2.src = img2;
-
-    // Product Preview Image 3
-    var photo3 = document.getElementById("img3");
-    photo3.src = img3;
-
-    // Product Preview Image 4
-    var photo4 = document.getElementById("img4");
-    photo4.src = img4;
-
-    // Product Preview Image 5
-    var photo5 = document.getElementById("img5");
-    photo5.src = img5;
-
-    // ---------------- Change Preview Image OnClick -------
-    function changeImage() {
-        // Photo 0
-        photo0.addEventListener("click", function() {
-            productImg.src = img0;
-        });
-
-        // Photo 1
-        photo1.addEventListener("click", function() {
-            productImg.src = img1;
-        });
-        // Photo 2
-        photo2.addEventListener("click", function() {
-            productImg.src = img2;
-        });
-
-        // Photo 3
-        photo3.addEventListener("click", function() {
-            productImg.src = img3;
-        });
-
-        // Photo 4
-        photo4.addEventListener("click", function() {
-            productImg.src = img4;
-        });
-
-        // Photo 5
-        photo5.addEventListener("click", function() {
-            productImg.src = img5;
-        });
-
-        // Toggle Active Class
-        $(document).on("click", ".previewImg img", function() {
-            $(this)
-                .addClass("active")
-                .siblings()
-                .removeClass("active");
-        });
+        isAccessory.appendChild(card);
 
     }
-    changeImage();
+    return card;
+}
+// for (i = 0; i <= productList.length; i++) {
+//     generateCard(productList[i]);
+// }
+
+var productArr = [];
+
+function renderCardGrid() {
+    for (i = 0; i < productArr.length; i++) {
+        generateCard(productArr[i]);
+
+    }
 }
 
-// ---------------- Insert Data into Local Storage > OnClick > Add To Cart Button -------
+xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/product", true);
+xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4) {
+        productArr = JSON.parse(xhttp.responseText);
+        // console.log(productArr);
+        renderCardGrid();
+    }
 
-var addToCartBtn = document.getElementById("add-to-cart");
-var cart = document.getElementById("cart-count");
+}
+xhttp.send();
+
+/*Creating product page*/
+
+var productBigImg = document.getElementById('productImg');
+
+var productName = document.getElementById("name");
+
+// Product Brand
+var productBrand = document.getElementById("brand");
+
+// Product Price
+var productPrice = document.getElementById("price");
+
+// Product Description
+var productDescription = document.getElementById("description");
+
+// Product Preview Image 0
+var photo0 = document.getElementById("img0");
+
+// Product Preview Image 1
+var photo1 = document.getElementById("img1");
+
+// Product Preview Image 2
+var photo2 = document.getElementById("img2");
+
+// Product Preview Image 3
+var photo3 = document.getElementById("img3");
+
+// Product Preview Image 4
+var photo4 = document.getElementById("img4");
+
+// Product Preview Image 5
+var photo5 = document.getElementById("img5");
+
+var currentObj = null;
+var addToCartBtn = document.get
+var productObj = [];
+
+var pageId = window.location.search.split('=')[1];
+// console.log(pageId);
+$.get('https://5d76bf96515d1a0014085cf9.mockapi.io/product/' + pageId, function(responseText, status) {
+    productObj = responseText;
+    currentObj = productObj;
+    // console.log('currentobj', currentObj);
+
+    productBigImg.src = productObj.preview;
+    productName.innerHTML = productObj.name;
+    productBrand.innerHTML = productObj.brand;
+    productPrice.innerHTML = productObj.price;
+    productDescription.innerHTML = productObj.description;
+    photo0.src = productObj.photos[0];
+    photo1.src = productObj.photos[1];
+    photo2.src = productObj.photos[2];
+    photo3.src = productObj.photos[3];
+    photo4.src = productObj.photos[4];
+
+
+
+    photo0.onclick = function() {
+        productBigImg.src = productObj.photos[0];
+    }
+    photo1.onclick = function() {
+        productBigImg.src = productObj.photos[1];
+    }
+    photo2.onclick = function() {
+        productBigImg.src = productObj.photos[2];
+    }
+    photo3.onclick = function() {
+        productBigImg.src = productObj.photos[3];
+    }
+    photo4.onclick = function() {
+        productBigImg.src = productObj.photos[4];
+    }
+    $(document).on("click", ".previewImg img", function() {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+    });
+});
+
+var addToCartBtn = document.getElementById('add-to-cart');
+var cart = document.getElementById('cart-count');
 var myCartData = [];
-var cartIntialValue;
+var cartInitialValue;
 
 if (localStorage.getItem('cart-count') == null) {
-    localStorage.setItem('cart-count', '0');
+    localStorage.setItem('cart-count', '0')
 } else {
     var cartValue = localStorage.getItem('cart-count');
     localStorage.setItem('cart-count', cartValue);
+
 }
+console.log('cartcount ' + localStorage.getItem('cart-count'));
 
-
-// ---------------- Increase Cart Count -----------------------
 function cartCount() {
-    if (window.localStorage.getItem("cart-count") === null) {
-        cartIntialValue = 0;
+    if (localStorage.getItem('cart-count') == null) {
+        cartInitialValue = 0;
+
     } else {
-        cartIntialValue = JSON.parse(window.localStorage.getItem("cart-count"));
-        cart.innerHTML = cartIntialValue;
+        cartInitialValue = JSON.parse(window.localStorage.getItem('cart-count'));
+        cart.innerHTML = cartInitialValue;
     }
-    var cartCurrentValue = cartIntialValue + 1;
-    window.localStorage.setItem("cart-count", cartCurrentValue);
+    var cartCurrentValue = cartInitialValue + 1;
+    window.localStorage.setItem('cart-count', cartCurrentValue);
     cart.innerHTML = window.localStorage.getItem("cart-count");
+
 }
 cart.innerHTML = window.localStorage.getItem("cart-count");
 
-// ---------------- Add Data into List and then into Local Storage -----------------------
-
 function addDataIntoList(productData) {
-    // If Local Storage Is Empty Then Set List To Empty
     if (window.localStorage.getItem("product-list") === null) {
         myCartData = [];
-    }
-    // If Local Storage Is Not Empty Then Set List To Value Of Local Storage
-    else {
+    } else {
         myCartData = JSON.parse(window.localStorage.getItem("product-list"));
-    }
 
-    // If List Is Empty Then Push The Object In It
+    }
     if (myCartData.length === 0) {
         var myObj = {
             id: productData.id,
@@ -294,39 +209,29 @@ function addDataIntoList(productData) {
             preview: productData.preview
         };
         myCartData.push(myObj);
-    }
-    // If List Is Not Empty Then
-    else if (myCartData.length != 0) {
+    } else if (myCartData.length != 0) {
         var w = 0;
-        // If Same Product Data == True Then List.Count++
-        for (var i = 0; i < myCartData.length; i++) {
-            if (myCartData[i].id == productData.id) {
-                myCartData[i].count = parseInt(myCartData[i].count) + 1;
-                w += 1;
-            }
-        }
-        // Else Add New Data Into List
-        if (w == 0) {
-            var myObj = {
-                id: productData.id,
-                title: productData.name,
-                count: 1,
-                price: productData.price,
-                preview: productData.preview
-            };
-            myCartData.push(myObj);
+        if (myCartData[i].id == productData.id) {
+            myCartData[i].count = parseInt(myCartData[i].count) + 1;
+            w += 1;
+
         }
     }
-    // Store The List Into Local Storage
+    if (w == 0) {
+        var myObj = {
+            id: productData.id,
+            title: productData.name,
+            count: 1,
+            price: productData.price,
+            preview: productData.preview
+        };
+        myCartData.push(myObj);
+    }
     window.localStorage.setItem("product-list", JSON.stringify(myCartData));
 }
-
-//------ Add-To-Cart-Btn Click Event Listner ------------------------
-
-addToCartBtn.addEventListener("click", function() {
+addToCartBtn.addEventListener('click', function() {
     var productId = window.location.search.split("=")[1];
-    var urlLink =
-        "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + productId;
+    var urlLink = "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + productId;
 
     function getDataForLocalStorage() {
         var http = new XMLHttpRequest();
@@ -337,12 +242,13 @@ addToCartBtn.addEventListener("click", function() {
                     addDataIntoList(productData);
                 }
             }
-        };
+        }
         http.open("GET", urlLink, true);
         http.send();
+
     }
     cartCount();
     getDataForLocalStorage();
-});
 
-//------------------------------------------------------------
+
+});
